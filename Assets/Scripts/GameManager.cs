4aff122ccfead5b99143a78fe9888a7b4fc98a03
Mneijndorff <33960298+Mneijndorff99@users.noreply.github.com>
2019-02-01 10:01:03 +0100

    GameManager added

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public List<GameObject> zombiesList;
+
+    private void Update()
+    {
+        if (zombiesList.Count < 0)
+            Debug.Log("GameEnded");
+    }
+
+}
