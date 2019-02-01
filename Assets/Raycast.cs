2b115a5335582b55e1bb3c525d96a7210ff71243
HoekigeHeer <ploxerdongaming@gmail.com>
2019-02-01 11:39:27 +0100

    Added shooting
    Attach to FPS controller, target must have tag "Zombie"

---
+++ Assets/Raycast.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Raycast : MonoBehaviour {
+   RaycastHit raycastHit;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey(KeyCode.A))
+        {
+            if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out raycastHit, Mathf.Infinity))
+            { if (raycastHit.collider.gameObject.tag == "Zombie")
+                {
+                    Destroy(raycastHit.collider.gameObject);
+                }
+            }
+        }
+	}
+}
